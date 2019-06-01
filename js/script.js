eba54ab0369002cebe5cdffa57c3c91e35aa95ae
Jodi <akajezebel@gmail.com>
2019-06-01 23:32:16 +0100

    Update script.js with menuToggle function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 var video = document.getElementById("myVideo");
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
