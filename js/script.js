1690f551ceb330d67e0d21cc1faf44dbd28cff2c
Jodi <akajezebel@gmail.com>
2019-06-02 15:58:52 +0100

    Update script.js to remove .responsive @media min-width 640

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ function menuToggle() {
     x.className = "navtoggle";
   }
 }
+
+function removeResponsive(y) {
+  if (y.matches) { // If media query matches
+    $( "#myNavtoggle" ).removeClass( "responsive" )
+  }
+}
+
+var y = window.matchMedia("(min-width: 640px)")
+removeResponsive(y) // Call listener function at run time
+y.addListener(removeResponsive) // Attach listener function on state changes
