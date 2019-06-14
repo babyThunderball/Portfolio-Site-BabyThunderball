9bb1848f0cab7b804682ef77d6e3301de47f1a9f
Jodi <akajezebel@gmail.com>
2019-06-14 16:00:10 +0100

    Update script.js to comment out toggle code so menu doesn't jump.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var video = document.getElementById("myVideo");
 
-function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
-  } else {
-    x.className = "navtoggle";
-  }
-}
+// function menuToggle() {
+//   var x = document.getElementById("myNavtoggle");
+//   if (x.className === "navtoggle") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navtoggle";
+//   }
+// }
 
 function removeResponsive(y) {
   if (y.matches) { // If media query matches
@@ -34,15 +34,15 @@ $('li.menuItem').click(function(){ //test to see if active state will work on cl
     .filter(this).addClass('active');
 })
 
-function removeColMd4(z) {
-  if (z.matches) { // If media query matches
-    $( ".left-col-skills").removeClass( "col-md-4 col-lg-4" )
-    .filter(this).addClass( "col-md-6 col-lg-6");
-    //$( ".right-col-skills").removeClass( "col-md-4 col-lg-4")
-    //.filter(this).addClass( "col-md-6 col-lg-6");
-  }
-}
-
-var y = window.matchMedia("(min-width: 640px)")
-removeResponsive(y) // Call listener function at run time
-y.addListener(removeResponsive) // Attach listener function on state changes
+// function removeColMd4(z) {
+//   if (z.matches) { // If media query matches
+//     $( ".left-col-skills").removeClass( "col-md-4 col-lg-4" )
+//     .filter(this).addClass( "col-md-6 col-lg-6");
+//     //$( ".right-col-skills").removeClass( "col-md-4 col-lg-4")
+//     //.filter(this).addClass( "col-md-6 col-lg-6");
+//   }
+// }
+//
+// var y = window.matchMedia("(min-width: 640px)")
+// removeResponsive(y) // Call listener function at run time
+// y.addListener(removeResponsive) // Attach listener function on state changes
