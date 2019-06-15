4a5bac572ecde7feaa555f6ee3798408728e2e91
Jodi <akajezebel@gmail.com>
2019-06-15 13:30:41 +0100

    Update script.js to uncomment toggle code so burger menu works again in mobile views.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var video = document.getElementById("myVideo");
 
-// function menuToggle() {
-//   var x = document.getElementById("myNavtoggle");
-//   if (x.className === "navtoggle") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "navtoggle";
-//   }
-// }
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
 
 function removeResponsive(y) {
   if (y.matches) { // If media query matches
